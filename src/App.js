55a23c44a7abf60bfc2b8fb25c78a0fab66ec30f
Wajih <hass_wajih@hotmail.com>
2022-04-05 11:27:32 -0400

    snapshottest

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import GitHubCard from './GitHubCard/GitHubCard'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App pt-5" >
+      <Row>
+        <Col style={{display: 'flex', justifyContent: 'center'}}>
+          <GitHubCard />
+        </Col>
+      </Row>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
+import renderer from 'react-test-renderer'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//This first test is commented out since we have the other snapshot test that checks the GitHubCard component
+test('renders a snapshot', () => {
+  const tree = renderer.create(<App/>).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+test('renders text MickeyMouse', () => {
+  render(<App />)
+  const nameElement = screen.getByText(/mickey mouse/i)
+  expect(nameElement).toBeInTheDocument()
 });
